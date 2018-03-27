e46799b898b6b9c3ec2cac31db4d354837feb631
take-a-CHANCE <chancejd53@gmail.com>
2018-03-27 11:59:31 -0400

    Basic framework for program

--- Main.py
+++ Main.py
@@ -11,5 +11,19 @@ strip    = Adafruit_DotStar(numpixels, datapin, clockpin)
 strip.begin()           # Initialize pins for output
 strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
 
+state=1
+
+while(1):
+    if(state==1):       # Waiting on input
+        state=2
+        exit()
+    if(state==2):       # Process changes to board
+        state=3
+        exit()
+    if(state==3):       # send updated board
+        state=1
+        exit()
+
+
 def updateBoard():
-    
+    exit()
