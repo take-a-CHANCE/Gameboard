f06caa41ae58caa0b926206801066e6a64e912ef
unknown <chancejd53@gmail.com>
2018-03-26 21:05:46 -0400

    Added Dotstar lib and main python file

---
+++ Main.py
@@ -0,0 +1,15 @@
+import time
+from dotstar import Adafruit_DotStar
+
+numpixels = 30 # Number of LEDs in strip
+
+# Here's how to control the strip from any two GPIO pins:
+datapin  = 23
+clockpin = 24
+strip    = Adafruit_DotStar(numpixels, datapin, clockpin)
+
+strip.begin()           # Initialize pins for output
+strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+
+def updateBoard():
+    
