8545581a01db93c799149172c41d4eac7be1da07
take-a-CHANCE <chancejd53@gmail.com>
2018-04-03 14:44:28 -0400

    Updated datastructure

--- Main.py
+++ Main.py
@@ -17,28 +17,25 @@ state=1
 # Board datastructure (array?)
 
 #  Row |    #   #   #   #   #   #   #   #   #
-#      |Col A   B   C   D   E   F   G   H  Status   
-#   1  |    0   1   2   3   4   5   6   7  79
-#   2  |    8   9  10  11  12  13  14  15  78
-#   3  |   16  17  18  19  20  21  22  23  77
-#   4  |   24  25  26  27  28  29  30  31  76
-#   5  |   32  33  34  35  36  37  38  39  75
-#   6  |   40  41  42  43  44  45  46  47  74
-#   7  |   48  49  50  51  52  53  54  55  73
-#   8  |   56  57  58  59  60  61  62  63  72
-# Stat |   64  65  66  67  68  69  70  71  **
+#      | Status A   B   C   D   E   F   G   H     
+#   1  |   79   0   1   2   3   4   5   6   7  
+#   2  |   78   8   9  10  11  12  13  14  15  
+#   3  |   77  16  17  18  19  20  21  22  23  
+#   4  |   76  24  25  26  27  28  29  30  31  
+#   5  |   75  32  33  34  35  36  37  38  39  
+#   6  |   74  40  41  42  43  44  45  46  47  
+#   7  |   73  48  49  50  51  52  53  54  55  
+#   8  |   72  56  57  58  59  60  61  62  63  
+# Stat |   **  64  65  66  67  68  69  70  71  
 
 
 while(1):
     if(state==1):       # Waiting on input
         state=2
-        exit()
-    if(state==2):       # Process changes to board
+    if(state==2):       # Process changes to board and display them
         state=3
-        exit()
     if(state==3):       # send updated board
         state=1
-        exit()
 
 
 def updateBoard():
