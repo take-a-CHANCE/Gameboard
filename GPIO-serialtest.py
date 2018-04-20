bdfc5c93ea7865241b2ebe52777777e35f50cfb0
take-a-CHANCE <chancejd53@gmail.com>
2018-04-19 20:57:15 -0400

    fixed a mistake

--- GPIO-serialtest.py
+++ GPIO-serialtest.py
@@ -12,7 +12,7 @@ ser=serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while 1:
-    if GPIO.input(channel):
+    if not GPIO.input(channel):
         ser.write("test\n")
         print("Wrote data\n")
     time.sleep(0.01)
