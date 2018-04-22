8c6d6c0f5156dc83b01aed11496304cd185e44ef
take-a-CHANCE <chancejd53@gmail.com>
2018-04-21 20:40:16 -0400

    testing test

--- GPIO-serialtest.py
+++ GPIO-serialtest.py
@@ -17,7 +17,7 @@ GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while 1:
     if not GPIO.input(channel):
-        ser.write('test'+'\n')
+        ser.write('test'+'\n','ascii')
         ser.flush()
         print "Wrote data\n"
     
