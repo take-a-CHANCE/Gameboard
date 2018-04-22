ac6be8ca4e6874c0cb3a05e15598c803e6d04adb
take-a-CHANCE <chancejd53@gmail.com>
2018-04-22 13:32:52 -0400

    changing to BCM #

--- GPIO-serialtest.py
+++ GPIO-serialtest.py
@@ -7,17 +7,17 @@ from dotstar import Adafruit_DotStar
 gridPixels = 30 # Number of LEDs in strip
 
 # Here's how to control the strip from any two GPIO pins:
-gridDatapin  = 28
-gridClockpin = 29
+gridDatapin  = 20
+gridClockpin = 21
 gridStrip    = Adafruit_DotStar(gridPixels, gridDatapin, gridClockpin)
 
 gridStrip.begin()           # Initialize pins for output
 gridStrip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
 
 
-channel=5
+channel=3
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 ser=serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
