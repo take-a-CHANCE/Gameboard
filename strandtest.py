cfa0a7c27400ee4ff94774f8f3de87b45c3e346c
take-a-CHANCE <chancejd53@gmail.com>
2018-04-24 18:22:37 -0400

    silly me

--- strandtest.py
+++ strandtest.py
@@ -13,8 +13,8 @@ from dotstar import Adafruit_DotStar
 numpixels = 72 # Number of LEDs in strip
 
 # Here's how to control the strip from any two GPIO pins:
-datapin  = 23
-clockpin = 24
+datapin  = 20
+clockpin = 21
 strip    = Adafruit_DotStar(numpixels, datapin, clockpin)
 
 # Alternate ways of declaring strip:
