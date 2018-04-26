7874f03a9ae1f7aa0d55a92b92f486c4215a85c6
take-a-CHANCE <chancejd53@gmail.com>
2018-04-25 22:42:24 -0400

    trying pull ups

--- buttTest.py
+++ buttTest.py
@@ -15,14 +15,14 @@ gridStrip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
 
 GPIO.setmode(GPIO.BCM)
 
-chanList=[23,24,4,5,6,7,8,9]
+chanList=[2,3,4,5,6,7,8,9]
 
-GPIO.setup(chanList, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(chanList, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    if not GPIO.input(23):
+    if not GPIO.input(2):
         print "Pressed 0"
-    elif not GPIO.input(24):
+    elif not GPIO.input(3):
         print "Pressed 1"
     elif not GPIO.input(4):
         print "Pressed 2"
